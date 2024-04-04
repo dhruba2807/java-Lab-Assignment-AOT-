7cd1d18282e0cdf64318fc444ef65f3afab811d6
Dhruba Mondal <126279399+dhruba2807@users.noreply.github.com>
2024-04-04 10:00:10 +0000

    change

---
+++ JAVA_CODE/java1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PrintName {
+    public static void main(String[] args) {
+        // Program to print my name
+        System.out.println("ChatGPT");
+    }
+}
